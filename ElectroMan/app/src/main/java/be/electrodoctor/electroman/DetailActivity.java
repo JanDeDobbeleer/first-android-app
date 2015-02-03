e5abf87a8902dac55210c56d6e1c8bac8746a5a1
Jan Joris <jan.de.dobbeleer@gmail.com>
2015-02-03 08:20:41 +0100

    - Detail styled - Button is "working"

--- ElectroMan/app/src/main/java/be/electrodoctor/electroman/DetailActivity.java
+++ ElectroMan/app/src/main/java/be/electrodoctor/electroman/DetailActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import be.electrodoctor.electroman.database.SQLiteHelper;
 import be.electrodoctor.electroman.model.RepairJob;
@@ -12,13 +13,32 @@ import be.electrodoctor.electroman.model.RepairJob;
 
 public class DetailActivity extends ActionBarActivity {
 
+    private SQLiteHelper dbHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_detail);
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainListViewFragment.JOB_ID);
-        SQLiteHelper dbHelper = new SQLiteHelper(this);
+        dbHelper = new SQLiteHelper(this);
+        assignJob(message);
+    }
+
+    private void assignJob(String message) {
         RepairJob job = dbHelper.getRepairJob(Long.parseLong(message));
+        assignText(R.id.Content_Name, job.getClient().getName());
+        assignText(R.id.Content_Description, job.getDescription());
+        assignText(R.id.Content_Street, job.getClient().getAddress().getStreet());
+        assignText(R.id.Content_Number, Integer.toString(job.getClient().getAddress().getNumber()));
+        assignText(R.id.Content_PostalCode, Integer.toString(job.getClient().getAddress().getPostalCode()));
+        assignText(R.id.Content_City, job.getClient().getAddress().getCity());
+    }
+
+    private void assignText(int id, String text){
+        TextView textView = (TextView)findViewById(id);
+        if(textView == null)
+            return;
+        textView.setText(text);
     }
 }
